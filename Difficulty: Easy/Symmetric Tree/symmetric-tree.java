e913ab110355cb52277ba752487f7898079d8de0
vennela343 <vennelamaisan@gmail.com>
2025-06-14 22:26:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Symmetric Tree/symmetric-tree.java
@@ -0,0 +1,32 @@
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+*/
+class Solution {
+    public boolean isSymmetric(Node root) {
+        // Code here
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+    
+    private boolean isMirror(Node t1, Node t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        if (t1.data != t2.data) return false;
+
+        return isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+}
+
+        
+ 
+    
