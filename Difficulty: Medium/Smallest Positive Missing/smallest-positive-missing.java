df8de1099d5834c9736e03c64967654cd7011855
vennela343 <vennelamaisan@gmail.com>
2025-07-22 21:48:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] arr) {
+        // code here
+        int n =1;
+        Arrays.sort(arr);
+        for(int num :arr){
+            if(num==n){
+                n++;
+            }
+        }
+        return n;
+    }
+}
+   
