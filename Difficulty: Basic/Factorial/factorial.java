31d5f007717fa6307ca965d58612eba24ae9cc24
vennela343 <vennelamaisan@gmail.com>
2025-08-03 17:22:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.java
@@ -0,0 +1,14 @@
+// User function Template for Java
+class Solution {
+    public static int nFactorial(int n) {
+        int ans = 1;
+
+        // Write your code here
+        for(int i=2;i<=n;i++){
+            ans *=i;
+        }
+        
+
+        return ans;
+    }
+}
